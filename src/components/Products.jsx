52df9d647d889af80393887acc364f026c0ab762
espadv69 <aitorjob69@gmail.com>
2024-08-08 13:48:49 +0200

    Update: - Items with other json

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,1 +1,30 @@
 import './css/Products.css'
+import { AddToCartIcon } from './icons/Icons.jsx'
+
+export function Products ({ products }) {
+  return (
+    <main className="products">
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            <img 
+              src={product.image} 
+              alt={product.title} 
+            />
+
+            <div className='products__strongs'>
+              <strong>{product.title}</strong>
+              <strong>${product.price}</strong>
+            </div>
+
+            <div>
+              <button style={{color: 'whitesmoke'}}>
+                <AddToCartIcon />
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
+}

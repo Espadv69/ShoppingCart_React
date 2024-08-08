4b28768e7e54e63b9c7dd9f4f862edd6ba5de60b
espadv69 <aitorjob69@gmail.com>
2024-08-08 13:51:58 +0200

    Update: - HOOKS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,30 @@
+import { products as initialProducts } from '../mocks/products.json'
+import { Products } from './Products'
+import { useState } from 'react'
+
 function App() {
+  const [products] = useState(initialProducts)
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0,
+  })
+
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      return (
+        product.price >= filters.minPrice &&
+        (
+          filters.category === 'all' ||
+          product.category === filters.category
+        )
+      )
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
+
   return (
-    <>
-      <h1>Hola mundo</h1>
-    </>
+    <Products products={filteredProducts} />
   )
 }
 

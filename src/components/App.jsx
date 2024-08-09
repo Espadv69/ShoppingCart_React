4870401e132c9d4044b770e5648b7a7bb5401c7a
espadv69 <aitorjob69@gmail.com>
2024-08-09 08:22:55 +0200

    Update: - Restructure of the component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,18 @@
 import { products as initialProducts } from '../mocks/products.json'
 import { Products } from './Products.jsx'
 import { Header } from './Header.jsx'
+import { useFilters } from './UseFilters.jsx'
 import { useState } from 'react'
 
 function App() {
   const [products] = useState(initialProducts)
-  const [filters, setFilters] = useState({
-    category: 'all',
-    minPrice: 0,
-  })
-
-  const filterProducts = (products) => {
-    return products.filter(product => {
-      return (
-        product.price >= filters.minPrice &&
-        (
-          filters.category === 'all' ||
-          product.category === filters.category
-        )
-      )
-    })
-  }
-
+  const { filterProducts, setFilters } = useFilters()
   const filteredProducts = filterProducts(products)
 
   return (
     <>
-    <Header changeFilters={setFilters} />
-    <Products products={filteredProducts} />
+      <Header changeFilters={setFilters} />
+      <Products products={filteredProducts} />
     </>
   )
 }

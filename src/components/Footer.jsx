2d4bac3b710d0afe82c37757fc0a079dc256a7d8
espadv69 <aitorjob69@gmail.com>
2024-08-09 08:51:36 +0200

    Update: - Show console debug

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import './css/Footer.css'
 
-export function Footer () {
+export function Footer ({ filters }) {
   return (
     <footer className="footer">
-      <h4>Prueba Técnica de React * <span>@midudev.</span></h4>
-      <h5>Shopping Cart con useContext & useReducer.</h5>
+      {
+        JSON.stringify(filters, null, 2)
+      }
+      {/* <h4>Prueba Técnica de React * <span>@midudev.</span></h4>
+      <h5>Shopping Cart con useContext & useReducer.</h5> */}
     </footer>
   )
 }
